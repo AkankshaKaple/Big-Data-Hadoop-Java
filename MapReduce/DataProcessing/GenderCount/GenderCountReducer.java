f67d82ab6a737dfbb8922a7ee9613278a0a9e93b
AkankshaKaple <akankshakaple@gmail.com>
2020-07-16 07:25:09 +0530

    Class containing Reducer logic

---
+++ MapReduce/DataProcessing/GenderCount/GenderCountReducer.java
@@ -0,0 +1,22 @@
+package com.learn.emp.GenderCount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class GenderCountReducer extends Reducer<Text, LongWritable, Text, LongWritable>{
+	
+	@Override
+	protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+		long sum = 0;
+		for (LongWritable value : values) {
+			sum = sum + value.get();
+			
+		}
+		context.write(key, new LongWritable(sum));
+	}
+
+}
